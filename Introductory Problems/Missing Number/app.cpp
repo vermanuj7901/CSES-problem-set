4f337a1e11995f35d810443620c0d66fb4151d4c
vermanuj7901 <anujravinderverma403@gmail.com>
2022-08-27 18:49:29 +0530

    update

--- Introductory Problems/Missing Number/app.cpp
+++ Introductory Problems/Missing Number/app.cpp
@@ -1,11 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-void solve(){
-
+void solve(ll sum, ll n){
+	ll actualSum = (n*(n+1))/2;
+	ll ans = actualSum - sum;
+	cout<<ans<<endl;
 }
 
-int main(){
-
-    return 0;
+int main() {
+	ll n;
+	cin>>n;
+    int arr[n];
+    ll sum = 0;
+    for(int i=0; i<(n-1); i++){
+    	cin>>arr[i];
+    	sum+=arr[i];
+    }
+    solve(sum, n);
+	return 0;
 }
